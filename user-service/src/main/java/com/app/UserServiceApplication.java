4ea44d474f216c1df3b87173a9207fbfaf0f4000
shrikant <ershrikant@gmail.com>
2025-08-21 08:06:28 +0530

    police-service

--- user-service/src/main/java/com/app/UserServiceApplication.java
+++ user-service/src/main/java/com/app/UserServiceApplication.java
@@ -2,8 +2,11 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.app"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
